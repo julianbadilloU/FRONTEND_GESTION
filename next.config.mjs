f557d0a949a0956bec6a191ef1f44115d356da51
FlacoAfk <julianmonj45@gmail.com>
2026-05-01 19:23:41 -0500

    config: actualiza next.config.mjs con patrones remotos y .gitignore
    - next.config.mjs: configuraci\u00f3n de Turbopack y patrones remotos de im\u00e1genes
    - .gitignore: agrega frontend.log a la lista de ignorados

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,30 @@ const nextConfig = {
   turbopack: {
     root: process.cwd(),
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.dog.ceo',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn2.thecatapi.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
